82caa225d65b21251e80630a6f2f0e0068a238ce
Nico <nicopasquariello@gmail.com>
2023-03-22 18:53:33 -0600

    added css to listContainer ul.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PlantCard from "./PlantCard"
+import "../styles/home.css";
 
 function Home({plantList}){
     const cards = plantList.map(plant => <PlantCard key={plant.id} plant={plant}/>)
--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "../styles/plantCard.css";
 
 function PlantCard({plant}){
     const {name, image, price} = plant
     return (
         <div id="card">
             <img src={image}/>
-            <h1>{name} {price}</h1>
+            <h3>{name} {price}</h3>
         </div>
     )
 }
