616d217d13ec1b6d472dede859074b4f6964c31b
Nico <nicopasquariello@gmail.com>
2023-03-25 14:28:11 -0600

    added addPlant.css, AddPlant componenet displays form to add new plant and ul to show what plants have been added by a user.

--- src/components/AddPlant.js
+++ src/components/AddPlant.js
@@ -1,8 +1,30 @@
 import React from "react";
-
+import "../styles/addPlant.css";
 function AddPlant(){
     return (
-        <h1>AddPlant Form Here</h1>
+        <div className="addPlantComp">
+        <h1>Add a plant to sell!</h1>
+
+        <div id="addForm">
+        <form>
+            <input type='text' placeholder="Image URL"/>
+            <br/>
+            <input type='text' placeholder="Plant Name"/>
+            <br/>
+            <input type='number' placeholder="Price"/>
+            <br/>
+            <textarea id="instructions" type='text' placeholder="Care Instructions"/>
+            <br/>
+            <button type="submit">Submit</button>
+        </form>
+        </div>
+
+        <h1>History of Added Plants</h1>
+        <ul id="prevAdd">
+            {/* User added plants here  */}
+        </ul>
+        </div>
+
     )
 }
 
