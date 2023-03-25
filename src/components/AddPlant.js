fa410d140709086ad6ed6b632e74934a421616eb
Nico <nicopasquariello@gmail.com>
2023-03-25 15:55:27 -0600

    made add plant form into a controlled form.

--- src/components/AddPlant.js
+++ src/components/AddPlant.js
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/addPlant.css";
 function AddPlant(){
+    const [image, setImage] = useState('');
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState();
+    const [instructions, setInstructions] = useState('');
+
     return (
         <div className="addPlantComp">
         <h1>Add a plant to sell!</h1>
 
         <div id="addForm">
         <form>
-            <input type='text' placeholder="Image URL"/>
+            <input 
+            type='text'
+            placeholder="Image URL"
+            value={image}
+            onChange={e => setImage(e.target.value)}/>
             <br/>
-            <input type='text' placeholder="Plant Name"/>
+            <input 
+            type='text' 
+            placeholder="Plant Name"
+            value={name}
+            onChange={e => setName(e.target.value)}/>
             <br/>
-            <input type='number' placeholder="Price"/>
+            <input 
+            type='number' 
+            placeholder="Price"
+            value={price}
+            onChange={e => setPrice(e.target.value)}/>
             <br/>
-            <textarea id="instructions" type='text' placeholder="Care Instructions"/>
+            <textarea 
+            id="instructions" 
+            type='text' 
+            placeholder="Care Instructions"
+            value={instructions}
+            onChange={e => setInstructions(e.target.value)}/>
             <br/>
             <button type="submit">Submit</button>
         </form>
